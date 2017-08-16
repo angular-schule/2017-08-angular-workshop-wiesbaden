594f59ff1a73918224790b2bac1152b1a2070079
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2017-08-16 11:24:09 +0200

    custom pipe: "addOne"

--- book-rating/src/app/app.module.ts
+++ book-rating/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BookComponent } from './book/book.component';
+import { AddOnePipe } from './add-one.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    BookComponent
+    BookComponent,
+    AddOnePipe
   ],
   imports: [
     BrowserModule,
