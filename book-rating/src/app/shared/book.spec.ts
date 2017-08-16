bece3ebf5a4c716f0585b527f69c21af7f662fb6
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2017-08-16 15:48:39 +0200

    my first state-driven unit-test

--- book-rating/src/app/shared/book.spec.ts
+++ book-rating/src/app/shared/book.spec.ts
@@ -1,7 +1,40 @@
-import {Book} from './book';
+import { Book } from './book';
 
-xdescribe('Book', () => {
-  it('should create an instance', () => {
-    expect(new Book('', '', '')).toBeTruthy();
+describe('Book', () => {
+  let book: Book;
+  beforeEach(() => {
+    book = new Book('', '', '', 0);
+  });
+
+  it('should rate up by 1', () => {
+    book.rating = 0;
+    book.rateUp();
+    expect(book.rating).toBe(1);
+  });
+
+  it('should rate down by 1', () => {
+    book.rating = 1;
+    book.rateDown();
+    expect(book.rating).toBe(0);
+  });
+
+  it('should not be allowed to have a rating greater than 5', () => {
+    book.rating = 5;
+    book.rateUp();
+    expect(book.rating).toBe(5);
+  });
+
+  it('should not be allowed to have a rating smaller than 0', () => {
+    book.rating = 0;
+    book.rateDown();
+    expect(book.rating).toBe(0);
+  });
+
+  // beispiel
+  it('should throw an exception', () => {
+    const callback = () => {
+      throw { boom: 'yeah!' };
+    };
+    expect(callback).toThrow();
   });
 });
--- book-rating/src/app/shared/book.ts
+++ book-rating/src/app/shared/book.ts
@@ -4,4 +4,16 @@ export class Book {
               public description: string,
               public rating = 0) {
   }
+
+  rateUp() {
+    if (this.rating < 5) {
+      this.rating++;
+    }
+  }
+
+  rateDown() {
+    if (this.rating > 0) {
+      this.rating--;
+    }
+  }
 }
