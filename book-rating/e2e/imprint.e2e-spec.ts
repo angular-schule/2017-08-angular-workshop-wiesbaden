0949bdc44bdd726e17249eb5c0c9d6c8e5a1f185
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2017-08-17 13:21:02 +0200

    e2e test verify imprint

---
+++ book-rating/e2e/imprint.e2e-spec.ts
@@ -0,0 +1,21 @@
+import { browser, element, by } from 'protractor';
+
+describe('R+V imprint', () => {
+
+  beforeAll(() => browser.ignoreSynchronization = true);
+
+  it('should display the correct board members', () => {
+
+    browser.get('https://www.ruv.de/impressum');
+    const content = element(by.css('.standard-content'));
+
+    expect(content.getText()).toContain('Dr. Edgar Martin');
+    expect(content.getText()).toContain('Frank-Henning Florian');
+    expect(content.getText()).toContain('Heinz-Jürgen Kallerhoff');
+    expect(content.getText()).toContain('Julia Merkel');
+    expect(content.getText()).toContain('Marc René Michallet');
+    expect(content.getText()).toContain('Peter Weiler');
+  });
+
+  afterAll(() => browser.ignoreSynchronization = false);
+});
