440f921b717a6f42775f81b36042bed359f5cb75
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2017-08-17 16:39:41 +0200

    use @ViewChild to access DOM elements OR Directives from template

--- book-rating/src/app/create-book/create-book.component.ts
+++ book-rating/src/app/create-book/create-book.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewChild } from '@angular/core';
 import { Book } from '../shared/book';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'br-create-book',
@@ -11,10 +12,14 @@ export class CreateBookComponent {
   // TODO: two-bay binding
   book = Book.empty();
 
+  @ViewChild('isbn') myIsbnNgModel: NgModel;
+
   @Output()
   bookCreated = new EventEmitter<Book>();
 
   add() {
+    console.log(this.myIsbnNgModel);
+
     this.bookCreated.emit(this.book);
     this.book = Book.empty();
   }
